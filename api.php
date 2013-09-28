db7736b5452e38b280f403808f42931cc01f29b9
Vladislavs Golubs <ensirius@ensemplix.ru>
2013-09-29 01:30:58 +0300

    Comments fix api.php

--- api.php
+++ api.php
@@ -1,6 +1,9 @@
 <?php
 
-/** Exception который переобразует ошибку в JSON формат, что бы разработчики могли легко дебажить. */
+ /** 
+  * Exception который переобразует ошибку в JSON формат, что бы 
+  * разработчики могли легко дебажить. 
+  */
 Class ApiException extends Exception {
 
     public function __construct($error) {
