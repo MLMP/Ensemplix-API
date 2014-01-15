a31535722c36aa9187a7e1e9bd2d7ad48468687a
Vladislavs Golubs <ensirius@ensemplix.ru>
2014-01-15 15:01:35 +0200

    Проверка регионов игрока Ensemplix/Ensemplix-API#4

--- app/controllers/v2/regions_controller.rb
+++ app/controllers/v2/regions_controller.rb
@@ -3,7 +3,7 @@ class V2::RegionsController < ApplicationController
   # GET /regions/
   # Информация о регионах.
   def index
-    @regions = Region.all
+    @regions = Region.where("world = ?", params[:world]) if params.has_key? :world
 
     if params.has_key? :offset
       @regions = @regions.offset(params[:offset])
@@ -11,6 +11,14 @@ class V2::RegionsController < ApplicationController
 
     @count = @regions.count()
     @regions = @regions.limit(100)
+
+    if @regions.nil?
+      render :json => {
+          :error => "Regions not found.",
+          :world => params[:world]
+      }, :status => 422
+      return;
+    end
 
     @regions.each do |region|
       region_name = region['region']
@@ -32,6 +40,7 @@ class V2::RegionsController < ApplicationController
         :regions => @regions,
         :count => @count,
         :offset => params[:offset],
+        :world => params[:world]
     }
 
   end
@@ -50,11 +59,6 @@ class V2::RegionsController < ApplicationController
     end
 
     @region = Region.where("region = ? AND world = ?", params[:region], params[:world]).take()
-    @owners = Owner.where("region = ? AND world = ?", params[:region], params[:world]).pluck("player")
-    @members = Member.where("region = ? AND world = ?", params[:region], params[:world]).pluck("player")
-    @flags = Flag.where("region = ? AND world = ?", params[:region], params[:world]).pluck("flag")
-    @children = Child.where("parent = ? AND world = ?", params[:region], params[:world]).pluck("child")
-    @parent = Child.select("parent").where("child = ? AND world = ?", params[:region], params[:world]).take()
 
     if @region.nil?
       render :json => {
@@ -64,6 +68,12 @@ class V2::RegionsController < ApplicationController
       }, :status => 422
       return;
     end
+
+    @owners = Owner.where("region = ? AND world = ?", params[:region], params[:world]).pluck("player")
+    @members = Member.where("region = ? AND world = ?", params[:region], params[:world]).pluck("player")
+    @flags = Flag.where("region = ? AND world = ?", params[:region], params[:world]).pluck("flag")
+    @children = Child.where("parent = ? AND world = ?", params[:region], params[:world]).pluck("child")
+    @parent = Child.select("parent").where("child = ? AND world = ?", params[:region], params[:world]).take()
 
     render :json => {
         :region => @region,
@@ -103,6 +113,17 @@ class V2::RegionsController < ApplicationController
       @regions = @regions.offset(params[:offset])
     end
 
+    if @regions.empty?
+      render :json => {
+          :error => "No regions found.",
+          :x => params[:x],
+          :z => params[:z],
+          :world => params[:world],
+          :offset => params[:offset]
+      }, :status => 422
+      return;
+    end
+
     @regions.each do |region|
       region_name = region['region']
       world = region['world']
@@ -119,16 +140,6 @@ class V2::RegionsController < ApplicationController
       end
     end
 
-    if @regions.empty?
-      render :json => {
-          :error => "No regions found.",
-          :x => params[:x],
-          :z => params[:z],
-          :world => params[:world]
-      }, :status => 422
-      return;
-    end
-
     render :json => {
         :regions => @regions,
         :x => params[:x],
@@ -143,38 +154,67 @@ class V2::RegionsController < ApplicationController
   # Информация о регионах игрока.
   def player
     if !params.has_key? :player
-      render json: {error: "Please provide region."}, :status => 422
+      render json: {error: "Please provide player."}, :status => 422
       return;
     end
 
-    if !params.has_key? :world
-      render json: {error: "Please provide world."}, :status => 422
-      return;
+    @regions = nil
+
+    if params.has_key? :type
+      if params[:type].eql?("owners")
+        @regions = Region.select("DISTINCT `regions`.*").joins("INNER JOIN `regions_owners` ON
+          (`regions_owners`.`region` = `regions`.`region` AND `regions_owners`.`world` =  `regions`.`world`)").where("`regions_owners`.`player` = ?", params[:player])
+      else
+        @regions = Region.select("DISTINCT `regions`.*").joins("INNER JOIN `regions_members` ON
+          (`regions_members`.`region` = `regions`.`region` AND  `regions_members`.`world` =  `regions`.`world`)").where("`regions_members`.`player` = ?", params[:player])
+      end
+    else
+      @regions = Region.select("DISTINCT `regions`.*").joins("LEFT JOIN `regions_owners` ON
+         (`regions_owners`.`region` = `regions`.`region` AND `regions_owners`.`world` =  `regions`.`world`)").
+          joins("LEFT JOIN `regions_members` ON (`regions_members`.`region` = `regions`.`region` AND
+          `regions_members`.`world` =  `regions`.`world`)").where("`regions_owners`.`player` = ?
+          OR `regions_members`.`player` = ?", params[:player], params[:player])
     end
 
-    @region = Region.where("region = ? AND world = ?", params[:region], params[:world]).take()
-    @owners = Owner.where("region = ? AND world = ?", params[:region], params[:world]).pluck("player")
-    @members = Member.where("region = ? AND world = ?", params[:region], params[:world]).pluck("player")
-    @flags = Flag.where("region = ? AND world = ?", params[:region], params[:world]).pluck("flag")
-    @children = Child.where("parent = ? AND world = ?", params[:region], params[:world]).pluck("child")
-    @parent = Child.select("parent").where("child = ? AND world = ?", params[:region], params[:world]).take()
+    @regions = @regions.where("`regions`.`world` = ?", params[:world]) if params.has_key? :world
 
-    if @region.nil?
+    if params.has_key? :offset
+      @regions = @regions.offset(params[:offset])
+    end
+
+    if @regions.empty?
       render :json => {
-          :error => "Region not found.",
+          :error => "Regions not found.",
           :region => params[:region],
           :world => params[:world]
       }, :status => 422
       return;
     end
 
+    @count = @regions.count(distinct: true)
+    @regions = @regions.limit(100)
+
+    @regions.each do |region|
+      region_name = region['region']
+      world = region['world']
+      region['owners'] = Owner.where("region = ? AND world = ?", region_name, world).pluck("player")
+      region['members'] = Member.where("region = ? AND world = ?", region_name, world).pluck("player")
+      region['flags'] = Flag.where("region = ? AND world = ?", region_name, world).pluck("flag")
+      region['children'] = Child.where("parent = ? AND world = ?", region_name, world).pluck("child")
+      parent = Child.select("parent").where("child = ? AND world = ?", region_name, world).take()
+
+      if !parent.nil?
+        region['parent'] = parent['parent']
+      else
+        region['parent'] = nil
+      end
+    end
+
     render :json => {
-        :region => @region,
-        :owners => @owners,
-        :members => @members,
-        :flags => @flags,
-        :children => @children,
-        :parent => @parent
+        :regions => @regions,
+        :count => @count,
+        :offset => params[:offset],
+        :world => params[:world]
     }
   end
 
